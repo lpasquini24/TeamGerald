3271f61c741ad5f3c1da9b60a4a72f168d491f04
lpasquini24 <123428916+lpasquini24@users.noreply.github.com>
2023-03-25 15:17:02 +0800

    Finished enemy ai script

--- Team_Gerald_Project/Assets/Scripts/EnemyBehavior.cs
+++ Team_Gerald_Project/Assets/Scripts/EnemyBehavior.cs
@@ -8,12 +8,19 @@ using Pathfinding;
 [RequireComponent(typeof(Seeker))]
 public class EnemyBehavior : MonoBehaviour
 {
+    //enums
+
+    //manages the state that the enemy is currently in
+    private enum EnemyState { Passive, Chase, Attack };
+
     //serialized parameters
 
     //when the enemy is this far from the current waypoint, they move on to the next waypoint
     [SerializeField] float nextWaypointDistance;
     //the transform that the enemy will pathfind towards when chasing
     [SerializeField] Transform chaseTarget;
+    //the speed at which the enemy will chase the target
+    [SerializeField] float chaseSpeed;
     
     //references
 
@@ -24,6 +31,13 @@ public class EnemyBehavior : MonoBehaviour
 
     //stores the current path the enemy is following
     private Path p;
+    //stores the waypoint that the enemy is located at
+    private int currentWaypoint = 0;
+    private bool pathCompleted = false;
+    //stores the state that the enemy is currently in
+    private EnemyState state = EnemyState.Passive;
+    //stores the time since a path has been generated
+    private float timeSincePath;
 
     
     void Start()
@@ -34,23 +48,71 @@ public class EnemyBehavior : MonoBehaviour
         //auto-assign the target to the player
         if (chaseTarget == null) chaseTarget = GameObject.FindWithTag("Player").transform;
 
-        //test generating a path
-        GeneratePath(chaseTarget);
+        //makes the enemy chase the player by default, for testing
+        state = EnemyState.Chase;
+        
     }
 
     //creates a path and stores the value in p
     void GeneratePath(Transform target)
     {
+        //generate a path
+        timeSincePath = 0f;
         p = seeker.StartPath(transform.position, target.position);
+    }
+
+    //applies a force pushing the rigidbody along the path
+    void FollowPath(float amount)
+    {
+        //check if path is null
+        if (p == null) return;
+        //check if the path is completed
+        if(currentWaypoint == p.vectorPath.Count)
+        {
+            pathCompleted = true;
+            return;
+        }
+        else
+        {
+            pathCompleted = false;
+        }
+        //find the direction to move in
+        Vector2 direction = ((Vector2)p.vectorPath[currentWaypoint+1] - rb.position).normalized;
+        //apply a force in that direction
+        rb.AddForce(amount * direction);
+        
+        //check if we've reached the next waypoint
+        if(Vector2.Distance(rb.position, (Vector2) p.vectorPath[currentWaypoint+1]) < nextWaypointDistance)
+        {
+            currentWaypoint++;
+        }
     }
     
     void Update()
     {
-        
+        //increment timers
+        timeSincePath += Time.deltaTime;
+
+        //the enemy state machine
+        switch (state)
+        {
+            case EnemyState.Passive:
+                break;
+            case EnemyState.Chase:
+                //generates a path towards the chase target (by default, the player)
+                //does not generate a path if one has been generated recently
+                if(timeSincePath>0.3f) GeneratePath(chaseTarget);
+                //move along the path at chasing speed
+                FollowPath(chaseSpeed * Time.deltaTime);
+                break;
+            case EnemyState.Attack:
+                break;
+        }
     }
 
     private void FixedUpdate()
     {
 
     }
+
 }
